12fc83f412a1fc9411d1ddadc78baaef958c425d
shaynMshemIAH <110634772+shaynMshemIAH@users.noreply.github.com>
2025-06-18 10:31:45 -0600

    Create pages.tsx

---
+++ pages.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AlgocracyJudge from '../AlgocracyJudge';
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-100 p-4">
+      <AlgocracyJudge />
+    </main>
+  );
+}
